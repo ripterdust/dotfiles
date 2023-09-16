a2f64c0216f9346ba6668450bb490ece383a1bac
ripterdust <bryantello2010@hotmail.com>
2023-09-16 11:01:19 -0600

    main, Changed keybinding

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,10 @@
 feh --bg-fill ~/.config/qtile/wallpaper.jpg
 picom --config ~/.config/picom/picom.conf &
 nm-applet &
-setxkbmap latam
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 # Polybar setting up
 # polybar first &
 # polybar second &
+
+setxkbmap latam
