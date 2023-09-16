9537b94cc12ebf5f986e5e53b9e7a728c612d13a
ripterdust <bryantello2010@hotmail.com>
2023-09-16 11:05:36 -0600

    main, Setted up functions

--- setup.sh
+++ setup.sh
@@ -1,56 +1,4 @@
 #!/bin/bash
-echo "Configuring alacritty..."
-rm -rf ~/.config/alacritty
-cp -r alacritty ~/.config/alacritty
-
-echo "Configuring dunst..."
-rm -rf ~/.config/dunst
-cp -r dunst ~/.config/dunst
-killall dunst
-dunst &
-
-echo "Configuring nvim..."
-rm -rf ~/.config/nvim
-cp -r nvim ~/.config/nvim
-
-echo "configuring picom..."
-rm -rf ~/.config/picom
-cp -r picom ~/.config/picom
-
-echo "Configuring polybar..."
-rm -rf ~/.config/polybar
-cp -r polybar ~/.config/polybar
-
-echo "Configuring qtile..."
-rm -rf ~/.config/qtile
-cp -r qtile ~/.config/qtile
-
-echo "Configuring rofi..."
-rm -rf ~/.config/rofi
-cp -r rofi ~/.config/rofi
-
-echo "Configuring scripts..."
-cp -r scripts ~/.local/bin
-sudo chmod +x ~/.local/bin/scripts/*
-
-fonts_folder="$HOME/.fonts"
-
-if [ ! -d "$fonts_folder" ]; then
-    echo "Creating the fonts folder..."
-    mkdir -p "$fonts_folder"
-else
-    echo "The .fonts folder already exists; no further action is needed."
-fi
-
-echo "Copying fonts..."
-cp -r fonts/* "$fonts_folder"
-
-echo "Installing needed packages..."
-sudo pacman -Sy rofi qtile dunst picom feh networkmanager polybar sddm
-
-echo "Configuring SDDM..."
-sudo systemctl enable sddm
-sudo systemctl start sddm
 
 config_alacritty(){
     echo "Installing alacritty"
@@ -72,6 +20,67 @@ config_alacritty(){
     scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
     cd ..
 }
+
+install_dependencies(){
+    echo "Installing needed packages..."
+    sudo pacman -Sy rofi qtile dunst picom feh networkmanager polybar sddm
+
+    echo "Configuring SDDM..."
+    sudo systemctl enable sddm
+    sudo systemctl start sddm
+}
+
+set_config_files(){
+    echo "Configuring alacritty..."
+    rm -rf ~/.config/alacritty
+    cp -r alacritty ~/.config/alacritty
+
+    echo "Configuring dunst..."
+    rm -rf ~/.config/dunst
+    cp -r dunst ~/.config/dunst
+    killall dunst
+    dunst &
+
+    echo "Configuring nvim..."
+    rm -rf ~/.config/nvim
+    cp -r nvim ~/.config/nvim
+
+    echo "configuring picom..."
+    rm -rf ~/.config/picom
+    cp -r picom ~/.config/picom
+
+    echo "Configuring polybar..."
+    rm -rf ~/.config/polybar
+    cp -r polybar ~/.config/polybar
+
+    echo "Configuring qtile..."
+    rm -rf ~/.config/qtile
+    cp -r qtile ~/.config/qtile
+
+    echo "Configuring rofi..."
+    rm -rf ~/.config/rofi
+    cp -r rofi ~/.config/rofi
+
+    echo "Configuring scripts..."
+    cp -r scripts ~/.local/bin
+    sudo chmod +x ~/.local/bin/scripts/*
+
+    fonts_folder="$HOME/.fonts"
+
+    if [ ! -d "$fonts_folder" ]; then
+        echo "Creating the fonts folder..."
+        mkdir -p "$fonts_folder"
+    else
+        echo "The .fonts folder already exists; no further action is needed."
+    fi
+
+    echo "Copying fonts..."
+    cp -r fonts/* "$fonts_folder"
+}
+
+set_config_files
+
+
 
 
 
