6dc0fde3bac011db075e77512da71ea1a0c5251c
ripterdust <bryantello2010@hotmail.com>
2023-09-16 10:59:14 -0600

    main, Configured script

--- setup.sh
+++ setup.sh
@@ -52,21 +52,26 @@ echo "Configuring SDDM..."
 sudo systemctl enable sddm
 sudo systemctl start sddm
 
-# echo "Installing alacritty"
-# git clone https://github.com/alacritty/alacritty.git
+config_alacritty(){
+    echo "Installing alacritty"
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+    sudo pacman -S cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python
+    cargo build --release
+    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+    infocmp alacritty
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+    sudo mkdir -p /usr/local/share/man/man1
+    sudo mkdir -p /usr/local/share/man/man5
+    scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+    scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+    scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+    scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+    cd ..
+}
 
-# cd alacritty
-# sudo pacman -S cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python
-# cargo build --release
-# sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-# infocmp alacritty
-# sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-# sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-# sudo desktop-file-install extra/linux/Alacritty.desktop
-# sudo update-desktop-database
-# sudo mkdir -p /usr/local/share/man/man1
-# sudo mkdir -p /usr/local/share/man/man5
-# scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-# scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-# scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
-# scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+setxkbmap latam
+
